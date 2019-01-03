917ce916feb493fae8a28f3d41e448aa6a7fc8e4
Modo Ltunzher <offnormal@gmail.com>
2019-01-03 23:53:13 +0200

    UI binded to location service status. Location and radius picking added

--- geofence/UI/MainViewController/MainViewController.swift
+++ geofence/UI/MainViewController/MainViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 import LocationPickerViewController
 import CoreLocation
 import CoreGeoFence
@@ -24,6 +26,8 @@ class MainViewController: UITableViewController {
         case radius
     }
     private let defaultCellId = "defaultCellId"
+
+    private var viewLifeDisposeBag: DisposeBag!
     private let locationService: LocationService
     
 // MARK: - Memory Management
@@ -41,6 +45,7 @@ class MainViewController: UITableViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         if self.viewIfLoaded?.window == nil {
+            self.viewLifeDisposeBag = nil
             self.view = nil
         }
     }
@@ -49,6 +54,37 @@ class MainViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.viewLifeDisposeBag = DisposeBag()
+
+        // Summary
+        let observableIsMonitoring = self.locationService.isMonitoringObservable
+        let observableRegionState = self.locationService.regionStateObservable
+        Observable.combineLatest(observableIsMonitoring, observableRegionState)
+            .subscribe(onNext: { [weak self] _, _ in
+                let indexPath = IndexPath(row: Row.summary.rawValue, section: 0)
+                self?.tableView?.reloadRows(at: [indexPath], with: .automatic)
+            }).disposed(by: self.viewLifeDisposeBag)
+
+        // Location services status
+        let observableStatus = self.locationService.serviceAuthorizationStatusObservable
+        let observableIsEnabled = self.locationService.serviceIsEnabledObservable
+        Observable.combineLatest(observableStatus, observableIsEnabled)
+            .subscribe(onNext: { [weak self] _, _ in
+                let indexPath = IndexPath(row: Row.locationStatus.rawValue, section: 0)
+                self?.tableView?.reloadRows(at: [indexPath], with: .automatic)
+            }).disposed(by: self.viewLifeDisposeBag)
+        
+        // Geofence region
+        let observableCenter = self.locationService.geofenceCenter.asObservable()
+        let observableRadius = self.locationService.geofenceRadius.asObservable()
+        Observable.combineLatest(observableCenter, observableRadius)
+            .subscribe(onNext: { [weak self] _, _ in
+                let indexPaths = [
+                    IndexPath(row: Row.coordinate.rawValue, section: 0),
+                    IndexPath(row: Row.radius.rawValue, section: 0)
+                ]
+                self?.tableView?.reloadRows(at: indexPaths, with: .automatic)
+            }).disposed(by: self.viewLifeDisposeBag)
     }
     
 // MARK: - Action Handlers
@@ -66,34 +102,107 @@ class MainViewController: UITableViewController {
     }
     
     private func checkRequestLocationPermission() {
+        guard self.locationService.serviceAuthorizationStatus == .notDetermined else {
+            let alert = UIAlertController(title: "Location Services", message: "Current status: \(self.locationService.serviceAuthorizationStatus.humanReadableDescription)", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Open Settings", style: .default, handler: { [weak self] _ in
+                self?.openApplictionSettins()
+            }))
+            alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+            return
+        }
         self.locationService.requestAlwaysAuthorization()
     }
     
     private func pickLocation(forcePick: Bool = false) {
-        // TODO
+        guard forcePick || self.locationService.geofenceCenter.value == nil else {
+            let actionSheet = UIAlertController(title: "Geofence center", message: "Change", preferredStyle: .actionSheet)
+            actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+            actionSheet.addAction(UIAlertAction(title: "Remove Region", style: .destructive, handler: { _ in
+                self.locationService.geofenceCenter.value = nil
+            }))
+            actionSheet.addAction(UIAlertAction(title: "Select New Center", style: .default, handler: { _ in
+                self.pickLocation(forcePick: true)
+            }))
+            self.present(actionSheet, animated: true, completion: nil)
+            return
+        }
+        let pickerVc = LocationPicker()
+        pickerVc.pickCompletion = { [weak self] (item) in
+            if let (lat, lon) = item.coordinate {
+                let coordinate = CLLocationCoordinate2D(latitude: lat, longitude: lon)
+                self?.updateGeofenceCenter(coordinate)
+            } else {
+                self?.updateGeofenceCenter(nil)
+            }
+        }
+        self.navigationController?.pushViewController(pickerVc, animated: true)
+    }
+    
+    private func updateGeofenceCenter(_ center: CLLocationCoordinate2D?) {
+        self.locationService.geofenceCenter.value = center
     }
     
     private func inputRadius() {
-        // TODO
+        let alert = UIAlertController(title: "Geofence Radius", message: "in meters", preferredStyle: .alert)
+        let actionOk = UIAlertAction(title: "OK", style: .default) { [weak alert, weak self] _ in
+            guard let numberString = alert?.textFields?.first?.text,
+                let number = Double(numberString),
+                number > 0 else {
+                return
+            }
+            self?.locationService.geofenceRadius.value = number
+        }
+        
+        alert.addTextField { textField in
+            textField.rx.text.orEmpty
+                .map { (Double($0) ?? 0) > 0 }
+                .share(replay: 1)
+                .bind(to: actionOk.rx.isEnabled)
+                .disposed(by: self.viewLifeDisposeBag)
+        }
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        alert.addAction(actionOk)
+        self.present(alert, animated: true, completion: nil)
     }
 
 // MARK: - Private (Cells)
     
     private func setupCellLocationStatus(_ cell: UITableViewCell) {
-        let statusString = "Location service access status"
+        let statusString = "Location service access status: \(self.locationService.serviceAuthorizationStatus.humanReadableDescription)"
+        let serviceIsEnabledString = "Service is enabled: \(self.locationService.serviceIsEnabled)"
         cell.textLabel?.text = statusString
+        cell.detailTextLabel?.text = serviceIsEnabledString
     }
     
     private func setupCellCoordinate(_ cell: UITableViewCell) {
-        cell.textLabel?.text = "Select Coordinate"
+        if let loc = self.locationService.geofenceCenter.value {
+            cell.textLabel?.text = "Change Coordinate"
+            let lat = String(format: "%.4f", loc.latitude)
+            let lon = String(format: "%.4f", loc.longitude)
+            cell.detailTextLabel?.text = "Coordinate: lat \(lat) lon \(lon)"
+        } else {
+            cell.textLabel?.text = "Select Coordinate"
+            cell.detailTextLabel?.text = nil
+        }
     }
     
     private func setupCellRadius(_ cell: UITableViewCell) {
         cell.textLabel?.text = "Radius"
+        let radius = self.locationService.geofenceRadius.value
+        cell.detailTextLabel?.text = radius > 0
+            ? "\(self.locationService.geofenceRadius.value) meters"
+            : "not set"
     }
     
     private func setupCellSummary(_ cell: UITableViewCell) {
-        cell.textLabel?.text = "Summary here"
+        var summary: String = "Region monitoring: "
+        summary += self.locationService.isMonitoring
+            ? "active"
+            : "off"
+        summary += "\n"
+        summary += "Region status: \(self.locationService.regionState?.humanReadableDescription ?? "no information")"
+        cell.textLabel?.text = summary
     }
 
 // MARK: - UITableViewDelegate
